fcd459c9118f9846e2541f6917a7fe00b7851d05
Manoel Valladão <valladao@gmail.com>
2024-02-18 10:57:30 -0300

    Add new year to commission system

--- admintc_prod/views/content/commision.php
+++ admintc_prod/views/content/commision.php
@@ -30,6 +30,7 @@
           <option value="2021">2021</option>
           <option value="2022">2022</option>
           <option value="2023">2023</option>
+          <option value="2024">2024</option>
         </select>
       </div>
       <button type="submit" class="btn btn-primary" ng-click="getCommision()">Buscar</button>
